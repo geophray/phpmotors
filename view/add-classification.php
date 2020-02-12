31da04476df2a149b1f8a41ef41a5f95d21f15d5
Jeff Maughan <geophray@gmail.com>
2020-02-12 14:27:02 -0700

    Create add-classification view
    Create view/add-classification.php with form to collect new
    classification names and submit it to the vehicles controller for
    processing and saving to the database.

---
+++ view/add-classification.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Classification | PHP Motors</title>
+    <link rel="stylesheet" href="/phpmotors/css/styles.css" media="screen">
+</head>
+
+<body>
+    <div class="site-wrapper">
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/common/header.php'; ?>
+        </header>
+        <nav>
+            <?php echo $navList; ?>
+        </nav>
+        <main>
+            <h1>Add New Vehicle Classification</h1>
+
+            <?php
+                if (isset($message)) {
+                    echo $message;
+                }
+            ?>
+
+            <form id="signin" class="user-management" action="/phpmotors/vehicles/index.php" method="post">
+                <div>
+                    <label for="classificationName">Classification Name</label>
+                    <input type="text" id="classificationName" name="classificationName" required aria-required="true">
+                </div>
+                <div>
+                    <input type="submit" name="submit" id="add-classification" value="Add Classification">
+                </div>
+                <!-- Add the action name - value pair -->
+                <input type="hidden" name="action" value="add-new-classification">
+            </form>
+            
+        </main>
+        <footer>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/common/footer.php'; ?>
+            <p>Last Updated: <?php 
+            date_default_timezone_set('UTC');
+            echo date ("d F , Y", getlastmod()); 
+            ?></p>
+        </footer>
+    </div>
+</body>
+
+</html>
