17029cf59e2cb2c78665d4b0160c8dcef9d1f02d
Jeff Maughan <geophray@gmail.com>
2020-02-12 17:41:07 -0700

    Create vehicles controller

--- vehicles/index.php
+++ vehicles/index.php
@@ -22,8 +22,8 @@ foreach ($classifications as $classification) {
 $navList .= '</ul>';
 
 // Build the dynamic drop-down select list of classifications from the database.
-$classificationList = '<select name="" id ="">';
-$classificationList .= '<option value="">--Please choose an option--</option>';
+$classificationList = '<select name="classificationID" id ="classificationID">';
+$classificationList .= '<option value="" disabled selected>--Please choose an option--</option>';
 foreach ($classifications as $classification) {
     $classificationList .= "<option value='" . $classification['classificationID'] . "'>" . $classification['classificationName'] . "</option>";
 }
@@ -44,15 +44,15 @@ switch ($action){
         break;
     case 'add-new-vehicle':  // Add new vehicle to the database
         // Filter and store the data
-        $classificationID = filter_input(INPUT_POST, 'classificationID');
-        $invMake = filter_input(INPUT_POST, 'invMake');
-        $invModel = filter_input(INPUT_POST, 'invModel');
-        $invDescription = filter_input(INPUT_POST, 'invDescription');
-        $invImage = filter_input(INPUT_POST, 'invImage');
-        $invThumbnail = filter_input(INPUT_POST, 'invThumbnail');
-        $invPrice = filter_input(INPUT_POST, 'invPrice');
-        $invStock = filter_input(INPUT_POST, 'invStock');
-        $invColor = filter_input(INPUT_POST, 'invColor');
+        $classificationID = filter_input(INPUT_POST, 'classificationID', FILTER_SANITIZE_NUMBER_INT);
+        $invMake = filter_input(INPUT_POST, 'invMake', FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_STRIP_BACKTICK | FILTER_FLAG_ENCODE_AMP);
+        $invModel = filter_input(INPUT_POST, 'invModel', FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_STRIP_BACKTICK | FILTER_FLAG_ENCODE_AMP);
+        $invDescription = filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_STRIP_BACKTICK | FILTER_FLAG_ENCODE_AMP);
+        $invImage = filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_URL);
+        $invThumbnail = filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_URL);
+        $invPrice = filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_INT);
+        $invStock = filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_INT);
+        $invColor = filter_input(INPUT_POST, 'invColor', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_BACKTICK | FILTER_FLAG_ENCODE_AMP);
 
         // Check for missing input
         if(empty($classificationID) || empty($invMake) || empty($invModel) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invColor)) {
@@ -63,14 +63,24 @@ switch ($action){
         }
 
         // Insert the data to the database
+        $regOutcome = regVehicle( $invName, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationID );
 
         // Check and report the result
+        if ($regOutcome === 1) {
+            $message = "The $invMake $invModel was added successfully!";
+            include '../view/add-vehicle.php';
+            exit;
+        } else {
+            $message = "<p>Registration of $invMake $invModel failed.  Please try again.</p>";
+            include '../view/add-vehicle.php';
+            exit;
+        }
 
         break;
     case 'add-new-classification': // Add new classification to the database
         // Filter and store the data
-        $classificationName = filter_input(INPUT_POST, 'new');
-
+        $classificationName = filter_input(INPUT_POST, 'classificationName', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_BACKTICK | FILTER_FLAG_ENCODE_AMP);
+        
         // Check for missing input
         if(empty($classificationName)) {
             $message = "<p>Please provide information for all empty form fields.</p>";
@@ -80,12 +90,21 @@ switch ($action){
         }
 
         // Insert the data to the database
+        $regOutcome = regClassification($classificationName);
 
         // Check and report the result
             // should return visitor to vehicle management view
+        if ($regOutcome === 1) {
+            include '../view/vehicle-man.php';
+            exit;
+        } else {
+            $message = "<p>Registration of classification " . $classificationName . " failed.  Please try again.</p>";
+            include '../view/add-classification.php';
+            exit;
+        }
 
         break;
     default:
-        include '../view/home.php';
+        include '../view/vehicle-man.php';
     }
 ?>
