9addc69bf5adcf34eda9a9147729cf68106d96d8
Jeff Maughan <geophray@gmail.com>
2020-02-12 17:42:41 -0700

    Creation of vehicle management page

---
+++ view/vehicle-man.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vehicle Management | PHP Motors</title>
+    <link rel="stylesheet" href="/phpmotors/css/styles.css" media="screen">
+</head>
+
+<body>
+    <div class="site-wrapper">
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/common/header.php'; ?>
+        </header>
+        <nav>
+            <?php echo $navList; ?>
+        </nav>
+        <main>
+            <h1>Vehicle Management</h1>
+            <ul>
+                <li><a href="../vehicles/index.php?action=new-classification">Add Classification</a></li>
+                <li><a href="../vehicles/index.php?action=new-vehicle">Add Vehicle</a></li>
+            </ul>
+        </main>
+        <footer>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/common/footer.php'; ?>
+            <p>Last Updated: <?php 
+            date_default_timezone_set('UTC');
+            echo date ("d F , Y", getlastmod()); 
+            ?></p>
+        </footer>
+    </div>
+</body>
+
+</html>
