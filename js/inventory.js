a734297d04269b4ae230325ebc5b7f09259471c7
Jeff Maughan <geophray@gmail.com>
2020-03-03 08:49:58 -0700

    Add buildInventoryList to event listener

--- js/inventory.js
+++ js/inventory.js
@@ -21,3 +21,24 @@ classificationList.addEventListener("change", function () {
             console.log('There was a problem: ', error.message)
         })
 })
+
+// Build inventory items into HTML table components and inject into DOM 
+function buildInventoryList(data) { 
+    let inventoryDisplay = document.getElementById("inventoryDisplay"); 
+    // Set up the table labels 
+    let dataTable = '<thead>'; 
+    dataTable += '<tr><th>Vehicle Name</th><td>&nbsp;</td><td>&nbsp;</td></tr>'; 
+    dataTable += '</thead>'; 
+    // Set up the table body 
+    dataTable += '<tbody>'; 
+    // Iterate over all vehicles in the array and put each in a row 
+    data.forEach(function (element) { 
+     console.log(element.invId + ", " + element.invModel); 
+     dataTable += `<tr><td>${element.invMake} ${element.invModel}</td>`; 
+     dataTable += `<td><a href='/phpmotors/vehicles?action=mod&id=${element.invId}' title='Click to modify'>Modify</a></td>`; 
+     dataTable += `<td><a href='/phpmotors/vehicles?action=del&id=${element.invId}' title='Click to delete'>Delete</a></td></tr>`; 
+    }) 
+    dataTable += '</tbody>'; 
+    // Display the contents in the Vehicle Management view 
+    inventoryDisplay.innerHTML = dataTable; 
+   }
